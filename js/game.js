dc8d9bd09c58e90abd536d7b1a54f4598d0e1a35
Eun Chong Ju <vevsing@gmail.com>
2020-03-15 14:07:29 +0900

    Update
    update

--- js/game.js
+++ js/game.js
@@ -22,6 +22,40 @@ window.oncontextmenu = function () {
 };
 
 
+// Create Flag
+function makeFlag() {
+	var flag = new CreativeFlag();
+	flag.setFlagFrame(1);
+	flag.setFlagDivision(2);
+	
+}
+// Creating Country Flag
+var CreativeFlag = function() {
+	var flagMemory = [
+		1,	// 가로나눔은 0, 세로나눔은 1, 중간 원(일본형)은 2, 3은 체코형, 4는 윈도형
+		2,	// 나누는 것으로는 단색은 1, 2색은 2, 3색은 3 : 3까지만 있음 (가로나눔을 몇번 나눌 것인가를 저장)/원형은 0, 체코형은 무조건 3임. 윈도형은 4임.
+		[5,3],	// 색깔을 저장; 위 또는 왼쪽부터 순서대로, 나누어진 수만큼 저장. 색깔은 고유 지정값에 기인. (체코형은 왼쪽 삼각형 먼저, 그다음은 위에서부터), (윈도형은 빨초노파 순서임: 2010년생 후는 아무도 모를듯)
+	];	// 색깔은 간단하다. (0:black;1:red;2:orange;3:yellow;4:green;5:skyblue;6:blue;7:puple;8:gray;9:white)
+	
+	this.setFlagFrame = function(set) {
+		flagMemory[0] = set;
+	};
+	this.setFlagDivision = function(num) {
+		flagMemory[1] = num;
+	};
+	this.selectFlagColors = function() {
+		var colors = new Array(flagMemory[1].length-1);
+		for (var i = 0; i < flagMemory[1].length; i++) {
+			colors[i] = i;	// 사용자가 선택을 해서 비동기적으로 값을 받는다.
+		}
+	};
+	
+	this.makeFlag = function() {
+		
+	};
+};
+
+
 // Country Random Naming
 function randomNamingCountry() {
 	document.getElementById('country').value = namingCountry();
@@ -68,7 +102,7 @@ var NameCountryMemory = function() {
 				max--;
 			}
 			
-			index = getRandomIntInclusive(min, max);
+			index = this.getRandomIntInclusive(min, max);
 			
 			arr[i] = index;
 		}
@@ -84,7 +118,9 @@ var NameCountryMemory = function() {
 					countryName += ' ';
 				}
 			}
+			
 			countryName += this.COUNTRY_MEM[j][arr[j]];
+			
 			if ((j==2) && (arr[j+1] != 0)) {
 				countryName += ' ';
 			}
@@ -93,13 +129,14 @@ var NameCountryMemory = function() {
 		return countryName;
 	};
 	
+	this.getRandomIntInclusive = function(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
+	};
 };
 
-function getRandomIntInclusive(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
-}
+
 
 // Select Level
 function selectLv(lv) {
@@ -124,7 +161,7 @@ function junctionLvSelect(lv) {
 	return attr;
 }
 
-
+/*
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() { myFunction() };
 
@@ -134,7 +171,7 @@ function myFunction() {
 	var scrolled = (winScroll / height) * 100;
 	document.getElementById("myBar").style.width = scrolled + "%";
 }
-
+*/
 
 
 
@@ -190,15 +227,87 @@ function validValueCheck(val) {
 	}
 }
 
+// Get the modal
+var modal = document.getElementById('editFlag');
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
+function openEditFlag() {
+	document.getElementById('editFlag').style.display='block';
+}
+function closeEditFlag() {
+	document.getElementById('editFlag').style.display='none';
+}
 
+function startSetFlag() {
+	hideAllFlag();
+	showFlag('horizon');
+}
 
+function hideAllFlag() {
+	hideFlag('horizon');
+	hideFlag('vertical');
+	hideFlag('czech');
+	hideFlag('circle');
+}
+function showFlag(id) {
+	hideAllFlag();
+	
+	switch (id) {
+		case 'horizon':
+		case 'vertical':
+		case 'windows':
+			showFlagFlex(id);
+			break;
+		case 'czech':
+		case 'circle':
+			showFlagBlock(id);
+			break;
+	}
+}
+function showFlagFlex(id){
+	for (let el of document.querySelectorAll('.'+id)) {
+		el.style.display = 'flex';
+	}
+}
+function showFlagBlock(id) {
+	for (let el of document.querySelectorAll('.'+id)) {
+		el.style.display = 'block';
+	}
+}
+function hideFlag(id) {
+	for (let el of document.querySelectorAll('.'+id)) el.style.display = 'none';
+}
 
+function divideFlag(num) {
+	// 플래그 안의 갯수를 분할한다. 그전에 먼저 다 숨기고 하나씩 드러내는 것이다.
+}
 
+function changeFlagColor(id, color) {
+	for (let el of document.querySelectorAll('.'+id)) el.style.background = color;
+}
 
-
-
+function showColorPalette(num) {
+	hideColorPalette(4);	// 팔레트를 전부 숨기고 다시 표시하도록 한다.
+	for (var i = 1; i <= num; i++) {
+		document.querySelector('.fc-rows:nth-child('+i+')').style.display = 'block';
+	}
+}
+function hideColorPalette(num) {
+	for (var i = 1; i <= num; i++) {
+		document.querySelector('.fc-rows:nth-child('+i+')').style.display = 'none';
+	}
+}
+/*
+.fc-rows:nth-child(1){
+	display:none;
+}
+*/
 
 
 
